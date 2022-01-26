ba11f69de33ba1fad45d0c0a6cc1ba8550296f84
Donovan <donovan.broquin@outlook.com>
2022-01-26 08:30:26 +0100

    init guests list and add bottom elements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+import chunk from 'lodash.chunk'
+
+const container = document.querySelector('#screen__content')
+const guests = chunk(JSON.parse(import.meta.env.VITE_GUESTS), 6)
 const controls = ['left', 'top', 'right', 'bottom']
+
+console.log(guests)
 
 controls.forEach(control =>
   document
@@ -6,9 +12,50 @@ controls.forEach(control =>
     .addEventListener('click', () => console.log('clicked', control))
 )
 
-function boot() {
-  let container = document.querySelector('#screen__content')
+function buildItem(guest) {
+  const el = document.createElement('li')
 
+  el.classList.add('guest__item')
+  el.innerHTML = `<svg
+  xmlns="http://www.w3.org/2000/svg"
+  class="guest__chevron"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M9 5l7 7-7 7"
+  />
+</svg>`
+  el.appendChild(document.createTextNode(guest.name))
+
+  return el
+}
+
+function showGuests() {
+  let currentChunkId = 0
+  let currentGuestId = 0
+
+  let guestsScreen = document.createElement('ul')
+  guestsScreen.classList.add('guests')
+
+  guests[currentChunkId]
+    .map(buildItem)
+    .map((guest, idx) => {
+      if (idx === currentGuestId) guest.classList.add('current')
+
+      return guest
+    })
+    .forEach(guest => guestsScreen.appendChild(guest))
+
+  container.innerHTML = ''
+  container.appendChild(guestsScreen)
+}
+
+function boot() {
   container.innerHTML = `<div id="boot">Chargement</div>`
 
   const content = document.querySelector('#boot')
@@ -27,6 +74,8 @@ function cleanLoading(loader, content) {
   clearInterval(loader)
 
   content.innerHTML = 'Chargement terminé'
+
+  setTimeout(showGuests, 600)
 }
 
 boot()
