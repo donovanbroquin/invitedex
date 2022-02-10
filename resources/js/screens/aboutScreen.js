4dd7c44b5d8c14692f389f3cc877afeb7eedba98
Donovan <donovan.broquin@outlook.com>
2022-02-10 23:50:46 +0100

    better about screen

--- resources/js/screens/aboutScreen.js
+++ resources/js/screens/aboutScreen.js
@@ -3,13 +3,27 @@ import { container } from '../helpers'
 export default function (globalEmitter) {
     container.innerHTML = ''
 
-    const text =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut dolor enim. Duis id ullamcorper velit. Praesent viverra tortor a sollicitudin sollicitudin. Sed eu nunc id arcu condimentum imperdiet vel non dolor'
+    const title = 'Invitédex'
+    const text = 'Cette application ne collecte aucune information.'
+    const textSecond =
+        'Les cookies présents sont nécessaires au fonctionnement et ne contient aucune donnée personnelle'
+
+    const aboutContainer = document.createElement('div')
+    aboutContainer.classList.add('about__container')
+
+    const titleEl = document.createElement('h1')
+    titleEl.innerHTML = title
 
     const paragraph = document.createElement('p')
     paragraph.innerHTML = text
 
-    container.appendChild(paragraph)
+    const paragraphSecond = document.createElement('p')
+    paragraphSecond.innerHTML = textSecond
+
+    aboutContainer.appendChild(titleEl)
+    aboutContainer.appendChild(paragraph)
+    aboutContainer.appendChild(paragraphSecond)
+    container.appendChild(aboutContainer)
 
     globalEmitter.on('ON_B_ABOUT', () =>
         globalEmitter.emit('CHANGE_SCREEN', { screen: 'menu' })
